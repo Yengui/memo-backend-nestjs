aa9c78d3604a622d611c58d3608a43914a74f07e
Yengui <80909453+Yengui@users.noreply.github.com>
2023-10-10 20:26:39 +0100

    guards

---
+++ src/guards/admin.guard.ts
@@ -0,0 +1,45 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
+import { Model } from 'mongoose';
+import { UserInterface } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel('User') private readonly User: Model<UserInterface>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      const isAdmin = (await this.User.findById(payload.sub)).isAdmin;
+      if (!isAdmin) {
+        throw new UnauthorizedException();
+      }
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
